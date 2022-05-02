8fe9a0f73dd38c7b17ee16eccc6971c600c437c2
Ander <shuqinz2@illinois.edu>
2022-05-01 23:41:10 -0500

    optimized

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,15 @@ use rocket::fs::{relative, FileServer};
 use rocket::form::Form;
 use rocket::response::stream::{EventStream, Event};
 use black_jack::message::Message;
-// use rocket::serde::{Serialize, Deserialize};
 use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
 use rocket::tokio::select;
 
 pub mod black_jack;
 use black_jack::runner::BlackJackRunner;
 
-// #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
-// #[serde(crate = "rocket::serde")]
-// struct Message {
-//     #[field(validate = len(..30))]
-//     pub room: String,
-//     #[field(validate = len(..20))]
-//     pub username: String,
-//     pub message: String,
-// }
-
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    unsafe { if STATE != 0 { state(STATE, form.message.clone()); }}
     let _res = queue.send(form.into_inner());
 }
 
@@ -33,10 +23,10 @@ fn game() {
     blackjack_runner.run();
 }
 
-static mut NUM: u32 = 0;
+static mut STATE: u32 = 0;
+static mut RESPOND: u32 = 0;
 static mut DECK: u32 = 0;
 static mut PLAYER: u32 = 0;
-// static mut dealer: Player = Player::new(String::from("Dealer"));
 
 #[get("/events")]
 async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
@@ -55,67 +45,91 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
             yield Event::json(&msg);
 
             unsafe {
-                if (NUM == 0 && msg.message == "blackjack") {
-                    NUM += 1;
-                    yield bot("Welcome to BlackJack! Enter \"quit\" to leave the game");
-                }
-                if (NUM != 0) {
-                    if (msg.message == "quit") {
-                        NUM = 0;
-                    } else {
-                        yield blackjack(NUM, msg.message);
-                        unsafe { if (NUM == 3) { 
-                            yield bot("How many players are playing? (1-7)");
-                            NUM += 1; }}
-                    }
-                }
+            if (RESPOND == 0 && msg.message == "blackjack") {
+                STATE = 1;
+                yield respond(1);
             }
+            if (RESPOND != 0) {
+                if (msg.message == "quit") {
+                    STATE = 10;
+                    yield respond(0);
+                } else {
+                    yield respond(RESPOND);
+                }
+            }}
         }
     }
 }
 
-fn blackjack(state: u32, input: String) -> Event {
-    if state == 1 {
-        unsafe {
-            NUM += 1;
-        }
-        bot("How many decks do you wanna use? (6-8)")
-    } else if state == 2 {
+fn state(state: u32, input: String) {
+    unsafe {
+    if state == 1 { // Start
+        STATE = 2;
+        RESPOND = 2;
+    } else if state == 2 { // Ask for number of decks
         match input.parse::<u32>() {
             Ok(val) => {
                 if val >= 6 && val <= 8 {
-                    unsafe {
-                        NUM += 1;
-                        DECK = val;
-                    }
-                    bot("####### Game Started! #######")
+                    STATE = 3;
+                    RESPOND = 3;
+                    DECK = val;
                 } else {
-                    bot("The number of decks must be between 6 and 8")
+                    RESPOND = 10;
                 }
             }
             Err(_) => {
-                bot("Expect integer input")
+                RESPOND = 5;
             }
         }
-    } else if state == 4 {
+    } else if state == 3 { // Game start
+        STATE = 4;
+        RESPOND = 4
+    } else if state == 4 { // Ask for number of players
         match input.parse::<u32>() {
             Ok(val) => {
                 if val >= 1 && val <= 7 {
-                    unsafe {
-                        NUM += 1;
-                        PLAYER = val;
-                        bot("")
-                    }
+                    STATE = 5;
+                    RESPOND = 5;
+                    PLAYER = val;
                 } else {
-                    bot("The number of players must be between 1 and 7")
+                    RESPOND = 12;
                 }
             }
             Err(_) => {
-                bot("Expect integer input")
+                RESPOND = 11;
             }
         }
+    } else if state == 5 { //
+
+    } else if state == 10 { // Abort
+        STATE = 0;
+        RESPOND = 0;
+    } else { // Unexpected error
+        RESPOND = 404;
+    }}
+}
+
+fn respond(state: u32) -> Event {
+    if state == 0 {
+        bot("Game aborted.")
+    } else if state == 1 {
+        bot("Welcome to BlackJack! Enter \"quit\" to leave the game")
+    } else if state == 2 {
+        bot("How many decks do you wanna use? (6-8)")
+    } else if state == 3 {
+        bot("####### Game Started! #######")
+    } else if state == 4 {
+        bot("How many players are playing? (1-7)")
+    } else if state == 5 {
+        bot("")
+    } else if state == 10 {
+        bot("The number of decks must be between 6 and 8")
+    } else if state == 11 {
+        bot("Expect integer input")
+    } else if state == 12 {
+        bot("The number of players must be between 1 and 7")
     } else {
-        bot("error!")
+        unsafe { bot(format!("Error! STATE: {}\tRESPOND: {}", STATE, RESPOND).as_str()) }
     }
 }
 
